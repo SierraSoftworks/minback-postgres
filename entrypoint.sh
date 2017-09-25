deebfe453390a76d3a0f538b4e9cbf9917de740f
Benjamin Pannell <admin@sierrasoftworks.com>
2017-09-25 21:06:57 +0100

    feat: Add support for passing a user and password via env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,24 @@
 set -e
 
 DB="$1"
+ARGS="${@:2}"
 
 mc config host add pg "$MINIO_SERVER" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" "$MINIO_API_VERSION" > /dev/null
 
 ARCHIVE="${MINIO_BUCKET}/${DB}-$(date $DATE_FORMAT).archive"
 
-echo "Dumping $DB to $ARCHIVE"
-echo "> pg_dump ${@:2} -F custom $DB"
+if [[ ! -z "$PGUSER" ]]; then
+    ARGS+="-U $PGUSER"
+fi
 
-pg_dump "${@:2}" -F custom "$DB" | mc pipe "pg/$ARCHIVE" || mc rm "pg/$ARCHIVE"
+if [[ ! -z "$PGPASSWORD" ]]; then
+    echo "*:*:*:*:${PGPASSWORD}" > /tmp/pgpass
+    export PGPASSFILE=/tmp/pgpass
+fi
+
+echo "Dumping $DB to $ARCHIVE"
+echo "> pg_dump ${ARGS} -F custom $DB"
+
+pg_dump "${ARGS}" -F custom "$DB" | mc pipe "pg/$ARCHIVE" || mc rm "pg/$ARCHIVE"
 
 echo "Backup complete"
