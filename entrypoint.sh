179afd098d2d4104f62944d9b333ec0e781d86ba
Benjamin Pannell <admin@sierrasoftworks.com>
2017-09-25 21:32:03 +0100

    fix: Correctly specify output format

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ mc config host add pg "$MINIO_SERVER" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" "$
 ARCHIVE="${MINIO_BUCKET}/${DB}-$(date $DATE_FORMAT).archive"
 
 echo "Dumping $DB to $ARCHIVE"
-echo "> pg_dump ${ARGS} -F custom $DB"
+echo "> pg_dump ${ARGS} -Fc $DB"
 
-pg_dump "${ARGS}" -F custom "$DB" | mc pipe "pg/$ARCHIVE" || { echo "Backup failed"; mc rm "pg/$ARCHIVE"; exit 1; }
+pg_dump "${ARGS}" -Fc "$DB" | mc pipe "pg/$ARCHIVE" || { echo "Backup failed"; mc rm "pg/$ARCHIVE"; exit 1; }
 
 echo "Backup complete"
