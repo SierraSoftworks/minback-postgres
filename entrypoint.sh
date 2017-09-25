7f73851515529c322b5e7151ea51ad3bbec5e003
Benjamin Pannell <admin@sierrasoftworks.com>
2017-09-25 21:28:09 +0100

    tweak: Remove unnecessary boilerplate

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,9 @@ mc config host add pg "$MINIO_SERVER" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY" "$
 
 ARCHIVE="${MINIO_BUCKET}/${DB}-$(date $DATE_FORMAT).archive"
 
-if [[ ! -z "$PGUSER" ]]; then
-    ARGS+="-U $PGUSER"
-fi
-
-if [[ ! -z "$PGPASSWORD" ]]; then
-    echo "*:*:*:*:${PGPASSWORD}" > /tmp/pgpass
-    export PGPASSFILE=/tmp/pgpass
-fi
-
 echo "Dumping $DB to $ARCHIVE"
 echo "> pg_dump ${ARGS} -F custom $DB"
 
-pg_dump "${ARGS}" -F custom "$DB" | mc pipe "pg/$ARCHIVE" || { echo "Backup failed"; mc rm "pg/$ARCHIVE"; exit 1 }
+pg_dump "${ARGS}" -F custom "$DB" | mc pipe "pg/$ARCHIVE" || { echo "Backup failed"; mc rm "pg/$ARCHIVE"; exit 1; }
 
 echo "Backup complete"
